2705440c0adc9f167176b22063cc9857da31a1e4
HackerU <hackeru@HackerUs-iMac.local>
2018-06-11 06:14:26 -0700

    final commit

--- Lec15/MLocationViewController.swift
+++ Lec15/MLocationViewController.swift
@@ -20,7 +20,30 @@ import UIKit
 import CoreLocation // CL
 
 class MLocationViewController: UIViewController {
-
+    @IBOutlet weak var searchTextField: UITextField!
+    @IBAction func search(_ sender: UIButton) {
+        guard let text = searchTextField.text else {return}
+        
+        geoCoder.geocodeAddressString(text) { (placemarks, err) in
+            if let err = err {
+                print(err)
+                return
+            }
+            
+            guard let placemark = placemarks?[0] else {return}
+                // code that runs in the background
+                // publish the result to the UI thread
+                DispatchQueue.main.async {
+                    let coord = placemark.location?.coordinate
+                    print(coord)
+                    self.searchTextField.text = "\(placemark)"
+                }
+            }
+        }
+    
+    
+    let geoCoder = CLGeocoder()
+    
     let lm = CLLocationManager() // the locatioManager Object
     
     override func viewDidLoad() {
